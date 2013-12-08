bcf0a94a315c667e721abd3828bada5da29b8bfb
devos1 <devos1@bluewin.ch>
2013-12-08 15:34:28 +0100

    Ajout Serie02Exo02 "ColorChooser"

---
+++ Serie02Exo02/Serie02Exo02/Form1.vb
@@ -0,0 +1,25 @@
+﻿'-------------------------------------------------------------------------------
+'Date		    : 08.12.2013                                                    '
+'Auteur		    : DevOs                                                         '
+'Nom			: Serie02Exo02                                                  '
+'Description	: Un simple ColorChooser qui affiche les valeur RGB             '
+'--------------------------------------------------------------------------------
+Public Class Form1
+
+    Private Sub vsbRed_Scroll(sender As Object, e As ScrollEventArgs) Handles vsbRed.Scroll
+        lblRvalue.Text = vsbRed.Value
+    End Sub
+
+    Private Sub vsbGreen_Scroll(sender As Object, e As ScrollEventArgs) Handles vsbGreen.Scroll
+        lblVvalue.Text = vsbGreen.Value
+    End Sub
+
+    Private Sub vsbBlue_Scroll(sender As Object, e As ScrollEventArgs) Handles vsbBlue.Scroll
+        lblBvalue.Text = vsbBlue.Value
+    End Sub
+
+    'PROCEDURE POUR COLORIER LE FOND DU PICTUREBOX
+    Private Sub colorPicBox(ByVal sender As Object, ByVal e As System.EventArgs) Handles vsbRed.ValueChanged, vsbGreen.ValueChanged, vsbBlue.ValueChanged
+        picColor.BackColor = Color.FromArgb(vsbRed.Value, vsbGreen.Value, vsbBlue.Value)
+    End Sub
+End Class
---
+++ Serie02Exo02/Serie02Exo02/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Serie02Exo02")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Philip Morris International")> 
+<Assembly: AssemblyProduct("Serie02Exo02")> 
+<Assembly: AssemblyCopyright("Copyright © Philip Morris International 2013")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("8147f98e-b688-4afd-bb9e-686dfa3d09ae")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
