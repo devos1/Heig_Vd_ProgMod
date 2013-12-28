51007b71ea4155c2c08dd79af952eade08b4a1d5
devos1 <devos1@bluewin.ch>
2013-12-28 15:57:32 +0100

    Ajout exercice Serie05Exo01

---
+++ Serie05Exo01/Serie05Exo01/Form1.Designer.vb
@@ -0,0 +1,98 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtContenu = New System.Windows.Forms.TextBox()
+        Me.btnPremier = New System.Windows.Forms.Button()
+        Me.btnPrecedent = New System.Windows.Forms.Button()
+        Me.btnSuivant = New System.Windows.Forms.Button()
+        Me.btnDernier = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtContenu
+        '
+        Me.txtContenu.Location = New System.Drawing.Point(12, 12)
+        Me.txtContenu.Multiline = True
+        Me.txtContenu.Name = "txtContenu"
+        Me.txtContenu.Size = New System.Drawing.Size(505, 44)
+        Me.txtContenu.TabIndex = 0
+        '
+        'btnPremier
+        '
+        Me.btnPremier.Location = New System.Drawing.Point(12, 79)
+        Me.btnPremier.Name = "btnPremier"
+        Me.btnPremier.Size = New System.Drawing.Size(104, 42)
+        Me.btnPremier.TabIndex = 1
+        Me.btnPremier.Text = "&First"
+        Me.btnPremier.UseVisualStyleBackColor = True
+        '
+        'btnPrecedent
+        '
+        Me.btnPrecedent.Location = New System.Drawing.Point(145, 79)
+        Me.btnPrecedent.Name = "btnPrecedent"
+        Me.btnPrecedent.Size = New System.Drawing.Size(104, 42)
+        Me.btnPrecedent.TabIndex = 2
+        Me.btnPrecedent.Text = "&Previous"
+        Me.btnPrecedent.UseVisualStyleBackColor = True
+        '
+        'btnSuivant
+        '
+        Me.btnSuivant.Location = New System.Drawing.Point(278, 79)
+        Me.btnSuivant.Name = "btnSuivant"
+        Me.btnSuivant.Size = New System.Drawing.Size(104, 42)
+        Me.btnSuivant.TabIndex = 3
+        Me.btnSuivant.Text = "&Next"
+        Me.btnSuivant.UseVisualStyleBackColor = True
+        '
+        'btnDernier
+        '
+        Me.btnDernier.Location = New System.Drawing.Point(411, 79)
+        Me.btnDernier.Name = "btnDernier"
+        Me.btnDernier.Size = New System.Drawing.Size(104, 42)
+        Me.btnDernier.TabIndex = 4
+        Me.btnDernier.Text = "&Last"
+        Me.btnDernier.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(528, 145)
+        Me.Controls.Add(Me.btnDernier)
+        Me.Controls.Add(Me.btnSuivant)
+        Me.Controls.Add(Me.btnPrecedent)
+        Me.Controls.Add(Me.btnPremier)
+        Me.Controls.Add(Me.txtContenu)
+        Me.Name = "Form1"
+        Me.Text = "Parcours d'un tableau statique"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtContenu As System.Windows.Forms.TextBox
+    Friend WithEvents btnPremier As System.Windows.Forms.Button
+    Friend WithEvents btnPrecedent As System.Windows.Forms.Button
+    Friend WithEvents btnSuivant As System.Windows.Forms.Button
+    Friend WithEvents btnDernier As System.Windows.Forms.Button
+
+End Class
---
+++ Serie05Exo01/Serie05Exo01/Form1.vb
@@ -0,0 +1,90 @@
+﻿Public Class Form1
+    '-------------------------------------------------------------------------------
+    'Date		    : 28.12.2013                                                    '
+    'Auteur		    : Oscar                                                         '
+    'Nom			: Serie05Exo01                                                  '
+    'Description	: Parcours d'un tableau à une dimension                         '
+    '--------------------------------------------------------------------------------
+
+    'CONSTANTES
+    Const MAX_TAB = 10
+
+    'GLOBAL_VAR POUR CETTE CLASSE
+    Dim tab(MAX_TAB) As String
+    Dim indiceEnCours As Integer
+
+    'INIT DU FORMULAIRE
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim i As Integer
+
+        'REMPLISSAGE DU TABLEAU
+        For i = 1 To MAX_TAB
+            If i = 1 Then
+                tab(i) = "Contenu du 1er élément"
+            Else
+                tab(i) = "Contenu du " & i & "ème élément"
+            End If
+        Next
+
+        'DONNE LE FOCUS AU BOUTON "PREMIER"
+        btnPremier.Focus()
+
+        'MODIFICATION PROPRIETES PAR DEFAUT
+        txtContenu.ReadOnly = True
+        txtContenu.TabStop = False
+    End Sub
+
+    '1ER ELEMENT
+    Private Sub btnPremier_Click(sender As Object, e As EventArgs) Handles btnPremier.Click
+
+        indiceEnCours = 1
+        modifieTextBox()
+        activeBtn()
+
+    End Sub
+
+    'ELEMENT PRECEDENT
+    Private Sub btnPrecedent_Click(sender As Object, e As EventArgs) Handles btnPrecedent.Click
+
+        indiceEnCours = indiceEnCours - 1
+        modifieTextBox()
+        activeBtn()
+
+    End Sub
+
+    'ELEMENT SUIVANT
+    Private Sub btnSuivant_Click(sender As Object, e As EventArgs) Handles btnSuivant.Click
+
+        indiceEnCours = indiceEnCours + 1
+        modifieTextBox()
+        activeBtn()
+
+    End Sub
+
+    'DERNIER ELEMENT
+    Private Sub btnDernier_Click(sender As Object, e As EventArgs) Handles btnDernier.Click
+
+        indiceEnCours = MAX_TAB
+        modifieTextBox()
+        activeBtn()
+
+    End Sub
+
+    'Insère dans le textbox l'élément du tableau concerné par l'indice
+    Private Sub modifieTextBox()
+
+        txtContenu.Text = tab(indiceEnCours)
+
+    End Sub
+
+    'Désactive les boutons quand on ne devrait plus pouvoir cliquer dessus
+    Private Sub activeBtn()
+
+        btnPremier.Enabled = indiceEnCours > 1
+        btnPrecedent.Enabled = indiceEnCours > 1
+        btnSuivant.Enabled = indiceEnCours < MAX_TAB
+        btnDernier.Enabled = indiceEnCours < MAX_TAB
+
+    End Sub
+End Class
