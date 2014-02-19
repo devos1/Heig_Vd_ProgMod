3fbf761cf598db739ec2c4333d7e0f907d7a0f01
devos1 <devos1@bluewin.ch>
2014-02-19 18:06:02 +0100

    Optimisation du code

--- Serie06Exo01/Serie06Exo01/Form1.vb
+++ Serie06Exo01/Serie06Exo01/Form1.vb
@@ -1,6 +1,6 @@
 ﻿'-------------------------------------------------------------------------------
 'Date		    : 07.12.2013                                                    '
-'Auteur		    : DevOs                                                         '
+'Auteur		    : Oscar                                                         '
 'Nom			: Serie06Exo01                                                  '
 'Description	: Formulaire de base                                            '
 '--------------------------------------------------------------------------------
@@ -8,89 +8,93 @@ Public Class Form1
 
     '---VAR_GLOBAL---
     'DECLARATION ET INIT D'UNE NOUVELLE COLLECTION
+    Private col As List(Of Personne) = New List(Of Personne)
     Dim c As Collection = New Collection()
     Dim noItemEnCours As Integer
 
+    'INIT DE LA COLLECTION DES PERSONNES
+    Private Sub initCol()
+        Dim p As Personne
+
+        'Constructeur 1
+        p = New Personne("nom1", "prenom1", New Date(1972, 12, 3))
+        p.photo = My.Resources.Smiley_01
+        col.Add(p)
+
+        'Constructeur 2
+        p = New Personne("nom2", "prenom2")
+        p.naissance = New Date(1980, 10, 1)
+        p.photo = My.Resources.Smiley_02
+        col.Add(p)
+
+        'Constructeur 3
+        p = New Personne("nom3", "prenom3", New Date(1988, 2, 16), My.Resources.Smiley_03)
+        col.Add(p)
+
+        'Constructeur 3
+        p = New Personne("nom4", "prenom4", New Date(1969, 1, 11), My.Resources.Smiley_04)
+        col.Add(p)
+
+        'Constructeur 3
+        p = New Personne("nom5", "prenom5", New Date(1991, 9, 6), My.Resources.Smiley_05)
+        col.Add(p)
+
+    End Sub
+
+    'LOAD DU FORM PAR DEFAUT
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-        Dim i As Integer
+        initCol()
+        txtNom.ReadOnly = True
+        txtNom.TabStop = False
+        picImgContact.BackgroundImageLayout = ImageLayout.Stretch
 
-        'CREATION DE i INSTANCES DE LA CLASSE PERSONNE ET AJOUT A LA COLLECTION C
-        For i = 1 To 5
-            Dim p As Personne = New Personne()
-            c.Add(p)
-        Next
+    End Sub
 
-        'INSERTION DES CONTACTS DANS LA COLLECTION
-        insertContact()
+    'PROCEDURE POUR METTRE A JOUR LES BOUTONS (CLIQUABLE OU PAS)
+    Private Sub updateButtons()
+        btnPremier.Enabled = noItemEnCours > 0
+        btnPrecedent.Enabled = noItemEnCours > 0
+        btnSuivant.Enabled = noItemEnCours < col.Count - 1
+        btnDernier.Enabled = noItemEnCours < col.Count - 1
+    End Sub
 
+    'PROCEDURE POUR INSERER LES INFOS DES CONTACTS DANS LE FORM
+    Private Sub insertInfosDansForm(ByVal p As Personne)
+        txtNom.Text = p.nom
+        txtPrenom.Text = p.prenom
+        txtAge.Text = p.age
+        picImgContact.Image = p.photo
     End Sub
 
     'PROCEDURE POUR AFFICHER LE PREMIER CONTACT DE LA COLLECTION
     Private Sub btnPremier_Click(sender As Object, e As EventArgs) Handles btnPremier.Click
-        insertInfosDansForm(1)
-        noItemEnCours = 1
+        noItemEnCours = 0
+        updateButtons()
+        insertInfosDansForm(col.Item(noItemEnCours))
     End Sub
 
     'PROCEDURE POUR AFFICHER LE DERNIER CONTACT DE LA COLLECTION
     Private Sub btnDernier_Click(sender As Object, e As EventArgs) Handles btnDernier.Click
-        Dim max As Integer
-        max = c.Count
-        insertInfosDansForm(max)
-        noItemEnCours = max
-    End Sub
-
-    'PROCEDURE POUR INSERER LES CONTACTS DANS LA COLLECTION DE PERSONNES
-    Private Sub insertContact()
-        c.Item(1).nom = "nom1"
-        c.Item(1).prenom = "prenom1"
-        c.Item(1).age = 20
-        c.Item(1).image = My.Resources.Smiley_01
-        c.Item(2).nom = "nom2"
-        c.Item(2).prenom = "prenom2"
-        c.Item(2).age = 21
-        c.Item(2).image = My.Resources.Smiley_02
-        c.Item(3).nom = "nom3"
-        c.Item(3).prenom = "prenom3"
-        c.Item(3).age = 22
-        c.Item(3).image = My.Resources.Smiley_03
-        c.Item(4).nom = "nom4"
-        c.Item(4).prenom = "prenom4"
-        c.Item(4).age = 23
-        c.Item(4).image = My.Resources.Smiley_04
-        c.Item(5).nom = "nom5"
-        c.Item(5).prenom = "prenom5"
-        c.Item(5).age = 24
-        c.Item(5).image = My.Resources.Smiley_05
-    End Sub
-
-    'PROCEDURE POUR INSERER LES INFOS DES CONTACTS DANS LE FORM
-    Sub insertInfosDansForm(num As Integer)
-        txtNom.Text = c.Item(num).nom
-        txtPrenom.Text = c.Item(num).prenom
-        txtAge.Text = c.Item(num).age
-        picImgContact.Image = c.Item(num).image
+        noItemEnCours = col.Count - 1
+        insertInfosDansForm(col.Item(noItemEnCours))
+        updateButtons()
     End Sub
 
     'PRODECURE POUR AVANCER DANS LA COLLECTION
     Private Sub btnSuivant_Click(sender As Object, e As EventArgs) Handles btnSuivant.Click
-        If noItemEnCours < c.Count Then
-            insertInfosDansForm(noItemEnCours + 1)
             noItemEnCours = noItemEnCours + 1
-        Else
-            MsgBox("Fin de la collection")
-        End If
+            updateButtons()
+            insertInfosDansForm(col.Item(noItemEnCours))
     End Sub
 
     'PRODECURE POUR RECULER DANS LA COLLECTION
     Private Sub btnPrecedent_Click(sender As Object, e As EventArgs) Handles btnPrecedent.Click
-        If noItemEnCours > 1 Then
-            insertInfosDansForm(noItemEnCours - 1)
+        If noItemEnCours > 0 Then
             noItemEnCours = noItemEnCours - 1
-        Else
-            MsgBox("Debut de collection")
+            updateButtons()
+            insertInfosDansForm(col.Item(noItemEnCours))
         End If
-
     End Sub
 End Class
 
--- Serie06Exo01/Serie06Exo01/Personne.vb
+++ Serie06Exo01/Serie06Exo01/Personne.vb
@@ -1,6 +1,6 @@
 ﻿'-------------------------------------------------------------------------------
 'Date		    : 07.12.2013                                                    '
-'Auteur		    : DevOs                                                         '
+'Auteur		    : Oscar                                                         '
 'Nom			: Serie06Exo01                                                  '
 'Description	: Classe Personne pour contenir les informations de personnes   '
 '--------------------------------------------------------------------------------
@@ -9,15 +9,48 @@ Public Class Personne
     'Attributs
     Private p_nom As String
     Private p_prenom As String
-    Private p_age As Integer
-    Private img As Image
+    Private p_ddn As Date
+    Private p_photo As Image
 
+#Region "CONSTRUCTEURS"
+    'Création contructeur 1
+    Public Sub New(ByVal n As String, ByVal p As String, ByVal ddn As String)
+        nom = n
+        prenom = p
+        naissance = ddn
+    End Sub
+
+    'Création contructeur 2
+    Public Sub New(ByVal n As String, ByVal p As String)
+        nom = n
+        prenom = p
+        naissance = Date.Today
+    End Sub
+
+    'Création contructeur 3
+    Public Sub New(ByVal n As String, ByVal p As String, ByVal ddn As String, ByVal p_photo As Image)
+        nom = n
+        prenom = p
+        naissance = ddn
+        photo = p_photo
+    End Sub
+
+    'Création contructeur 4
+    Public Sub New(ByVal n As String, ByVal p As String, ByVal ddn As String, ByVal chemin As String)
+        nom = n
+        prenom = p
+        naissance = ddn
+        photo = Image.FromFile(chemin)
+    End Sub
+#End Region
+
+#Region "PROPERTY"
     Public Property nom() As String
         Get
             Return p_nom
         End Get
         Set(value As String)
-            p_nom = value
+            p_nom = value.ToUpper()
         End Set
     End Property
 
@@ -26,28 +59,44 @@ Public Class Personne
             Return p_prenom
         End Get
         Set(ByVal value As String)
-            p_prenom = value
+            p_prenom = value.ToLower
         End Set
     End Property
 
-    Public Property age() As Integer
+    'Property pour la date de naissance avec contrôle que la date soit inférieur à today!
+    Public Property naissance() As Date
         Get
-            Return p_age
+            Return p_ddn
         End Get
-        Set(ByVal value As Integer)
-            p_age = value
+        Set(ByVal value As Date)
+            If (Date.Compare(value, Date.Today) < 0) Then
+                p_ddn = value
+            End If
         End Set
     End Property
 
-    Public Property image() As Image
+    Public Property photo() As Image
         Get
-            Return img
+            Return p_photo
         End Get
         Set(ByVal value As Image)
-            img = value
+            p_photo = value
         End Set
     End Property
 
+    'Property pour retourner l'âge en fonction de la date de naissance
+    Public ReadOnly Property age() As String
+        Get
+            Return Date.Now.Subtract(p_ddn).Days \ 365
+        End Get
+    End Property
 
+#End Region
+
+#Region "FONCTIONS - PROCEDURES"
+    Public Overrides Function ToString() As String
+        Return nom & " " & prenom
+    End Function
+#End Region
 
 End Class
