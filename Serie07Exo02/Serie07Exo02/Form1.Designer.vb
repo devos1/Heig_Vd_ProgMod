c0a12f4d438c7475d92530f9e0e2449f9187c5e6
devos1 <devos1@bluewin.ch>
2014-02-20 23:37:51 +0100

    Initial commit

---
+++ Serie07Exo02/Serie07Exo02/Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(43, 12)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(149, 199)
+        Me.ListBox1.TabIndex = 0
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.Location = New System.Drawing.Point(348, 12)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(149, 199)
+        Me.ListBox2.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(242, 25)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(57, 44)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "->"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(242, 149)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(57, 44)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "<-"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(541, 222)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Form1"
+        Me.Text = "Transfert de listes"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents ListBox2 As System.Windows.Forms.ListBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+
+End Class
---
+++ Serie07Exo02/Serie07Exo02/Form1.vb
@@ -0,0 +1,46 @@
+﻿'-------------------------------------------------------------------------------
+'Date		    : 20.02.2014                                                    '
+'Auteur		    : Oscar                                                         '
+'Nom			: Serie07Exo02                                                  '
+'Description	: Formulaire de base                                            '
+'--------------------------------------------------------------------------------
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ListBox1.Sorted = True
+        ListBox2.Sorted = True
+        ListBox1.SelectionMode = SelectionMode.MultiExtended
+        ListBox2.SelectionMode = SelectionMode.MultiExtended
+
+        ListBox1.Items.Add("Madrid")
+        ListBox1.Items.Add("Rome")
+        ListBox1.Items.Add("Tokyo")
+        ListBox1.Items.Add("Porto")
+        ListBox1.Items.Add("Paris")
+        ListBox1.Items.Add("Neuchâtel")
+        ListBox1.Items.Add("Lausanne")
+        ListBox1.Items.Add("New York")
+    End Sub
+
+    'PROCEDURE POUR TRANSFERER LES ELEMENTS SELECTIONNES DE LA LISTE SOURCE VERS LA LISTE DESTINATION
+    Private Sub transfert(ByVal lbSrc As ListBox, ByVal lbDest As ListBox)
+
+        Dim e As Object
+
+        While (lbSrc.SelectedItems.Count > 0)
+            e = lbSrc.SelectedItems.Item(0)
+            lbDest.Items.Add(e)
+            lbSrc.Items.Remove(e)
+        End While
+    End Sub
+
+    'TRANSFERT DE LA GAUCHE VERS LA DROITE
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        transfert(ListBox1, ListBox2)
+    End Sub
+
+    'TRANSFERT DE DROITE VERS LA GAUCHE
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        transfert(ListBox2, ListBox1)
+    End Sub
+End Class
